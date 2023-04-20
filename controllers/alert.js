6dde28de6b84a97e5355ab34f050b2169d016245
=GV= <g.sync.mobile1@gmail.com>
2023-04-21 07:47:48 +0800

    add postOptionsFactory for axios requests and destructure incoming inputs from request body

--- controllers/alert.js
+++ controllers/alert.js
@@ -2,31 +2,41 @@ import axios from 'axios';
 
 // todo: create factory function to create options object
 
-const options = {
-  method: 'POST',
-  url: 'https://api.humanloop.com/v3/generate',
-  headers: {
-    accept: 'application/json',
-    'Content-Type': 'application/json',
-    'X-API-KEY': 'hl_sk_e60a6fa9b619b25193743dd4a64e6391ec2f595b127b0b56',
-    // 'X-API-KEY': process.env.HUMANLOOP_API_KEY,
-  },
-  data: {
-    project: 'BRT',
-    inputs: {
-      grossSales: '42069',
-      txnCount: '696',
-      timeIn: '9:45',
-      currentTime: '15:29',
+const postOptionsFactory = (grossSales, txnCount, timeIn) => {
+  // initialize new date object
+  const now = new Date();
+  // return options object for axios request
+  return {
+    method: 'POST',
+    url: 'https://api.humanloop.com/v3/generate',
+    headers: {
+      accept: 'application/json',
+      'Content-Type': 'application/json',
+      'X-API-KEY': 'hl_sk_e60a6fa9b619b25193743dd4a64e6391ec2f595b127b0b56',
+      // 'X-API-KEY': process.env.HUMANLOOP_API_KEY,
     },
-    provider_api_keys: {
-      openai: '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',
-      // openai: process.env.OPENAI_API_KEY,
+    data: {
+      project: 'BRT',
+      inputs: {
+        grossSales,
+        txnCount,
+        timeIn,
+        currentTime: `${now.getHours()}:${now.getMinutes()}`,
+      },
+      provider_api_keys: {
+        openai: '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',
+        // openai: process.env.OPENAI_API_KEY,
+      },
     },
-  },
+  };
 };
 
 const getAlertGPT4 = async (req, res) => {
+  // destructure inputs from request body
+  const { grossSales, txnCount, timeIn } = req.body;
+  // create options object for axios request
+  const options = postOptionsFactory(grossSales, txnCount, timeIn);
+
   try {
     // make request to gpt4 from humanloop api
     const gptResponse = await axios.request(options);
