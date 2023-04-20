d757887e6679412154ff599abbaf9d5cfef2fa22
=GV= <g.sync.mobile1@gmail.com>
2023-04-20 21:40:04 +0800

    normalize response data for app integration

--- controllers/alert.js
+++ controllers/alert.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+// todo: create factory function to create options object
+
 const options = {
   method: 'POST',
   url: 'https://api.humanloop.com/v3/generate',
@@ -7,6 +9,7 @@ const options = {
     accept: 'application/json',
     'Content-Type': 'application/json',
     'X-API-KEY': 'hl_sk_e60a6fa9b619b25193743dd4a64e6391ec2f595b127b0b56',
+    // 'X-API-KEY': process.env.HUMANLOOP_API_KEY,
   },
   data: {
     project: 'BRT',
@@ -18,15 +21,22 @@ const options = {
     },
     provider_api_keys: {
       openai: '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',
+      // openai: process.env.OPENAI_API_KEY,
     },
   },
 };
 
 const getAlertGPT4 = async (req, res) => {
-  console.log('inside getAlertGPT4');
   try {
+    // make request to gpt4 from humanloop api
     const gptResponse = await axios.request(options);
-    res.status(200).json({ data: gptResponse.data });
+    // destructure output from gptResponse
+    const { output } = gptResponse.data.data[0];
+
+    res.status(200).json({
+      // split output by newline and filter out empty strings
+      gptData: output.split('\n').filter((val) => val !== ''),
+    });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
